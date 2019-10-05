a1bff2a9178e81e433292379263f05063dc006db
Marc <muenmac@googlemail.com>
2019-10-05 22:13:13 +0200

    casting tut

--- casting.py
+++ casting.py
@@ -1,3 +1,22 @@
 #casting.py
 
-# test for using github via terminal
+
+# integer casting
+x = int(8)
+y = int(2.8)
+z= int("42")
+print(x, y, z, type(y))
+print("y is" , type(y))
+
+# float casting
+
+a = float(8)
+b = float("33.5")
+print(a, b, type(b))
+
+# string casting
+
+f = str(33)
+g = str(42.7)
+print(f, g, type(f))
+
