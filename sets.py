5eb36732786395367fd743abc6d2cc984856fab1
Marc <muenmac@googlemail.com>
2019-10-06 00:04:24 +0200

    some sets...

---
+++ sets.py
@@ -0,0 +1,16 @@
+# sets.py
+
+# every index is unique, not sortable, not indexed
+
+my_set = {'one','two', 'three', 4, 'one'}
+print(my_set)
+
+# create unique data sets without duplicates
+
+# add and remove function
+
+my_set.add('five')
+print(my_set)
+
+my_set.remove(4)
+print(my_set)
