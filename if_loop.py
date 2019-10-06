ed4883d86627527ceeb9c2b4c0c0659eb7ae60f7
Marc <muenmac@googlemail.com>
2019-10-06 20:12:04 +0200

    some loops

---
+++ if_loop.py
@@ -0,0 +1,14 @@
+# if_loop.py
+
+
+a=5
+b=3
+c=7
+
+# note: loops is stopped after the first true value, other true values arent recognized anymore
+if a > b:
+    print("true")
+elif b<c:
+    print("true2")
+else:
+    print("false")
